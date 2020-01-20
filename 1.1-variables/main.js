ff70110a385cc096929b6fc67a9aeab0225cd67a
Небыкова Виктория <nebykova@office.local>
2020-01-20 16:52:51 +0300

    first commit variables

---
+++ 1.1-variables/main.js
@@ -0,0 +1,22 @@
+// Задача 1
+let algebra = 4;
+let geography = 5;
+let physics = 4;
+
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+//Задача 2
+
+let myName = 'Татьяна';
+let message = `Привет, мир! Меня зовут ${myName}.`
+console.log(message);
+
+// Задача 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
