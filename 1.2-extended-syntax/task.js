44eba557f9b5065aa1d10a80ee825b2edf69a0b7
Небыкова Виктория <nebykova@office.local>
2020-01-31 14:51:58 +0300

    Доработки

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -12,14 +12,14 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     let x = [];
-    let D = Math.sqrt (b**2 - 4*a*c);
-    let xFirst = (-b + D)/2*a;
-    let xSecond = (-b - D)/2*a;
+    let d = Math.sqrt (b**2 - 4*a*c);
+    let xFirst = (-b + d)/(2*a);
+    let xSecond = (-b - d)/(2*a);
 
-    if (D > 0) {
+    if (d > 0) {
         x.push(xFirst);
         x.push(xSecond); 
-    } else if (D === 0) {
+    } else if (d === 0) {
         x.push(xFirst);
     };
 
@@ -44,8 +44,7 @@ function getAverageMark(marks){
     for (let i = 0; i < marksList.length; i++) {
         sum += marksList[i];
     };
-    let averageMark = sum / marksList.length;
-    return averageMark;
+    return sum / marksList.length;
 }
 
 function calculateDrinkTask(){
@@ -57,9 +56,7 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     let today = new Date();
-    let todayYear = today.getFullYear();
-    let birthYear = dateOfBirthday.getFullYear();
-    let age = todayYear - birthYear;
+    let age = today.getFullYear(); - dateOfBirthday.getFullYear();
     let result;
 
     if (age >= 18) {
